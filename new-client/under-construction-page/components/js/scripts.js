ff16bd3b9d43704d878090f9d20858b1c23453d9
Robert Means <robert@robertmeans.com>
2018-07-26 15:32:09 -0400

    all kinds of stuff
    changed directory names
    updated robertmeans.com

--- new-client/under-construction-page/components/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function rUSure() {
-	if (confirm("Are you sure you want to delete all of the information you have entered?")) {
-		document.forms[0].reset();
-		document.forms[0].name.focus();
-}	else	{
-
-			}
-}
