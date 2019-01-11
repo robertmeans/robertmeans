4edd3a3c3ab8f42bcc2f4a2568226641f7c47c54
Evergreen Web Design <info@evergreenwebdesign.com>
2019-01-11 14:58:26 -0700

    added health folder with Anthem card

---
+++ website-email/health/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Anthem 2019</title>
+	<link rel="stylesheet" type="text/css" href="styles.css?<?php echo time(); ?>" />
+</head>
+<body>
+	<div class="wrap">
+		<img class="image" src="_images/anthem-health-insurance-card-2019.jpg" alt="Anthem 2019">
+	</div>
+</body>
+</html>
