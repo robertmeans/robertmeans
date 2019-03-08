7672dd714225149bf67b77e30597f34591e515db
Evergreen Web Design <info@evergreenwebdesign.com>
2019-03-08 15:05:58 -0700

    added health information

--- website-email/health/index.php
+++ website-email/health/index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Anthem 2019</title>
+	<title>Health Information 2019</title>
 	<link rel="stylesheet" type="text/css" href="styles.css?<?php echo time(); ?>" />
 </head>
 <body>
-	<div class="wrap">
-		<img class="image" src="_images/anthem-health-insurance-card-2019.jpg" alt="Anthem 2019">
+	<div id="wrap">
+		<a href="_images/anthem-health-insurance-card-2019.jpg" target="_blank">Insurance Card</a> | <a href="_images/robert-means_paperwork.pdf" target="_blank">Dr. Adam Smith - Paperwork</a>
 	</div>
 </body>
 </html>
