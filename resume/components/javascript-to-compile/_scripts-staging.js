93861d7a4b65ecff4eb4dc5751715cdd76abbcbf
Evergreen Web Design <info@evergreenwebdesign.com>
2020-03-12 13:10:08 -0600

    fixed highlighted nav
    fixed so that cover page is highlighted as default. changes made in JS to accommodate.

--- resume/components/javascript-to-compile/_scripts-staging.js
+++ resume/components/javascript-to-compile/_scripts-staging.js
@@ -1,25 +1,23 @@
-
-  $(window).scroll(function(e){ 
-    var $magicNav = $('#navigation-wrapper'); 
-    // var isPositionFixed = ($magicNav.css('position') == 'fixed');
-
-    if ($(this).scrollTop() > 160){ 
-      $magicNav.removeClass('nav-absolute');
-      $magicNav.addClass('nav-fixed'); 
-    }
-    if ($(this).scrollTop() < 160){
-      $magicNav.removeClass('nav-fixed');
-      $magicNav.addClass('nav-absolute'); 
+$(document).ready(function() {
+  $('[href]').each(function() {
+    if ((this.href == window.location.href) && !$('#cover-pg').hasClass('curent-page')) {
+      $('#cover-pg').removeClass('current-page');
+      $(this).addClass('current-page');  
     } 
-
   });
+});
 
 
-  $(document).ready(function() {
-    $("[href]").each(function() {
-        if (this.href == window.location.href) {
-            $(this).addClass("current-page");
-            
-        }
-    });
+$(window).scroll(function(e){ 
+  var $magicNav = $('#navigation-wrapper'); 
+
+  if ($(this).scrollTop() > 160){ 
+    $magicNav.removeClass('nav-absolute');
+    $magicNav.addClass('nav-fixed'); 
+  }
+  if ($(this).scrollTop() < 160){
+    $magicNav.removeClass('nav-fixed');
+    $magicNav.addClass('nav-absolute'); 
+  } 
+
 });
--- resume/cover.php
+++ resume/cover.php
@@ -7,6 +7,7 @@
 		<?php require('_includes/cv-header.php'); ?>
 		<div id="cover-body">
 			<p class="date"><?php echo date("l, F d, Y") ?></p>
+			<!-- <p class="covid">*Coronavirus free!*</p> -->
 			<p class="open">Hello,</p>
 			<p class="body-copy">In full discolsure, this snazzy presentation is deliberately intended as a decoy. It aims to spotlight the care and attention I invest in my work while gracefully masking a dearth of traditional résumé fodder.</p>
 			<p class="body-copy">Now that we're off to an honest start, I am wide open to entertaining virtually <i>any</i> business ideas. Please do not let a history of Web development typecast my skill set. The aptitude required for this field of work combined with personality, motivation and spirit translates into a broad range of potential.</p>
