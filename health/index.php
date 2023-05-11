f5286f02887a20b7eedd65ae358cf96253a2fa66
Robert Means <robert@robertmeans.com>
2023-05-10 19:36:26 -0600

    added eye expam

--- health/index.php
+++ health/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<div id="wrap">
-		<a href="_images/medicaid-card_2021.pdf" target="_blank">Insurance Card 2022</a> <span>|</span> <a href="_images/01-07-21_vision-prescription.pdf" target="_blank">Glasses</a> <span> | </span><a href="history/">History</a> | <a href="_images/Robert-Means_covid-card.pdf" target="_blank">Covid Card</a>
+		<a href="_images/medicaid-card_2021.pdf" target="_blank">Insurance Card 2022</a> <span>|</span> &nbsp;[ Eyes: <a href="_images/01-07-21_vision-prescription.pdf" target="_blank">01.07.21</a> - <a href="_images/May-10-2023-eye-prescription.pdf" target="_blank">05.10.23</a> ]&nbsp; <span> | </span><a href="history/">History</a> | <a href="_images/Robert-Means_covid-card.pdf" target="_blank">Covid Card</a>
 	</div>
 </body>
 </html>
