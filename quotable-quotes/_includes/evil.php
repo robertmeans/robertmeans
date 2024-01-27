30d508b825ad3117c7daf01f4282f02784690a9c
Robert Means <robert@robertmeans.com>
2024-01-27 09:36:26 -0700

    added evil section

---
+++ quotable-quotes/_includes/evil.php
@@ -0,0 +1,10 @@
+<div class="quote-content">
+  <ol class="quote-list">
+    <li>&quot;Men are more ready to repay an injury than a benefit, because gratitude is a burden and renvenge a pleasure.&quot; - Tactitus, A.D. 55-120</li>
+
+    <li>&quot;Two souls, alas, are housed within my breast, And each will wrestle for the mastery there.&quot; - Goethe, Faust, 1806</li>
+
+    <li>&quot;...The line separating good and evil passes not through states, nor between classes, nor between political parties either — but right through every human heart — and through all human hearts.&quot; - Alexander Solzhenitsyn, The Gulag Archipelago, 1968</li>
+    
+  </ol>
+</div>
